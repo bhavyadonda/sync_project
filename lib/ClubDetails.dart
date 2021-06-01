2933508e1088c9d8f1788610f7593054beb0c014
bhavyadonda <72097552+bhavyadonda@users.noreply.github.com>
2021-06-01 18:52:24 +0530

    Follow & Following button functionality added

--- lib/ClubDetails.dart
+++ lib/ClubDetails.dart
@@ -5,7 +5,6 @@ import 'package:adobe_xd/page_link.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
-
 import 'methods.dart';
 
 class ClubDetails extends StatefulWidget {
@@ -16,12 +15,25 @@ class ClubDetails extends StatefulWidget {
 class _ClubDetailsState extends State<ClubDetails> {
   String uid;
   String clubkey;
+  bool following;
 
   getdata() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     uid = prefs.getString('uid');
     Map data = json.decode(prefs.getString('clubs'));
+    Map userdata = json.decode(prefs.getString('userData'));
+    var followdata = userdata['following'].values.toList();
     clubkey = prefs.getString('clubdetails');
+    if (followdata.contains(clubkey)) {
+      setState(() {
+        following = true;
+      });
+    } else {
+      setState(() {
+        following = false;
+      });
+    }
+    print(followdata);
     return data;
   }
 
@@ -144,7 +156,6 @@ class _ClubDetailsState extends State<ClubDetails> {
                               await SharedPreferences.getInstance();
                               Map userdata =
                               json.decode(prefs.getString('userData'));
-
                               final databaseReference =
                               FirebaseDatabase.instance.reference();
 
@@ -162,11 +173,14 @@ class _ClubDetailsState extends State<ClubDetails> {
                                       .child(clubkey)
                                       .remove();
                                   userdata['following'].remove(clubkey);
+                                  setState(() {
+                                    following = !following;
+                                  });
 
                                   Navigator.pop(context);
                                   showAlertDialog(
                                       context,
-                                      '/Home',
+                                      '',
                                       'unfollowed Successfully',
                                       'You will now not be notified for the club.');
                                 } else {
@@ -176,11 +190,14 @@ class _ClubDetailsState extends State<ClubDetails> {
                                       .child(clubkey)
                                       .set(clubkey);
                                   userdata['following'][clubkey] = clubkey;
+                                  setState(() {
+                                    following = !following;
+                                  });
 
                                   Navigator.pop(context);
                                   showAlertDialog(
                                       context,
-                                      '/Home',
+                                      '',
                                       'following Created Successfully',
                                       'You will now be notified for the club.');
                                 }
@@ -207,7 +224,8 @@ class _ClubDetailsState extends State<ClubDetails> {
                               prefs.setString(
                                   'userData', json.encode(userdata));
                             },
-                            child: Container(
+                            child: following == true ?
+                            Container(
                               width: 87.0,
                               height: 28.0,
                               decoration: BoxDecoration(
@@ -235,6 +253,26 @@ class _ClubDetailsState extends State<ClubDetails> {
                                   textAlign: TextAlign.left,
                                 ),
                               ),
+                            ) :
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                SvgPicture.string(
+                                  _svg_k2ckhg,
+                                  allowDrawingOutsideViewBox: true,
+                                  fit: BoxFit.fill,
+                                ),
+                                Text(
+                                  'Follow',
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 11,
+                                    color: const Color(0xff404040),
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
                             ),
                           ),
                         ],
@@ -866,3 +904,5 @@ const String _svg_gzdh7g =
     '<svg viewBox="72.0 730.0 8.4 16.0" ><path transform="translate(-8.0, 730.0)" d="M 85.42222595214844 16 L 85.42222595214844 8.711111068725586 L 87.91111755371094 8.711111068725586 L 88.26667785644531 5.8666672706604 L 85.42222595214844 5.8666672706604 L 85.42222595214844 4.088889122009277 C 85.42222595214844 3.288889169692993 85.68890380859375 2.666667222976685 86.84445190429688 2.666667222976685 L 88.35556030273438 2.666667222976685 L 88.35556030273438 0.08888889104127884 C 88 0.08888889104127884 87.11111450195313 0 86.13333129882813 0 C 84 0 82.4888916015625 1.333333373069763 82.4888916015625 3.733333110809326 L 82.4888916015625 5.866666793823242 L 80 5.866666793823242 L 80 8.711111068725586 L 82.4888916015625 8.711111068725586 L 82.4888916015625 16 L 85.42222595214844 16 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
 const String _svg_see9ki =
     '<svg viewBox="32.0 48.0 12.6 18.0" ><defs><filter id="shadow"><feDropShadow dx="0" dy="3" stdDeviation="6"/></filter></defs><path transform="matrix(0.0, 1.0, -1.0, 0.0, 44.56, 48.0)" d="M 8.999999046325684 12.56341361999512 L 0 2.883406400680542 L 2.680851221084595 0 L 8.999999046325684 6.796600818634033 L 15.31914710998535 0 L 18 2.883406400680542 L 8.999999046325684 12.56341361999512 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" filter="url(#shadow)"/></svg>';
+const String _svg_k2ckhg =
+    '<svg viewBox="0.0 0.0 87.0 27.5" ><defs><linearGradient id="gradient" x1="0.017181" y1="0.087972" x2="0.984024" y2="0.895426"><stop offset="0.0" stop-color="#fffe4f70"  /><stop offset="1.0" stop-color="#ffcb6bd8"  /></linearGradient></defs><path transform="translate(0.0, 0.0)" d="M 73.2239990234375 27.54991149902344 L 13.7754020690918 27.54991149902344 C 10.0959358215332 27.54991149902344 6.636658191680908 26.11714553833008 4.034813404083252 23.51555633544922 C 1.432924628257751 20.91388893127441 2.453613205943839e-06 17.45476722717285 2.453613205943839e-06 13.77541160583496 C 2.453613205943839e-06 10.09598922729492 1.432924628257751 6.636711120605469 4.034813404083252 4.034822463989258 C 6.636702537536621 1.432933568954468 10.09598064422607 1.130506734625669e-05 13.7754020690918 1.130506734625669e-05 L 73.2239990234375 1.130506734625669e-05 C 76.90342712402344 1.130506734625669e-05 80.36270141601563 1.432933568954468 82.96459197998047 4.034822463989258 C 85.56648254394531 6.636711120605469 86.99940490722656 10.09598922729492 86.99940490722656 13.77541160583496 C 86.99940490722656 17.45476722717285 85.56648254394531 20.91388893127441 82.96459197998047 23.51555633544922 C 80.36274719238281 26.11714553833008 76.90347290039063 27.54991149902344 73.2239990234375 27.54991149902344 Z M 13.70430278778076 1.968311309814453 C 7.193846702575684 1.968311309814453 1.897202491760254 7.264955520629883 1.897202491760254 13.77541160583496 C 1.897202491760254 20.28586769104004 7.193846702575684 25.58251190185547 13.70430278778076 25.58251190185547 L 73.29510498046875 25.58251190185547 C 79.80555725097656 25.58251190185547 85.10220336914063 20.28586769104004 85.10220336914063 13.77541160583496 C 85.10220336914063 7.264955520629883 79.80555725097656 1.968311309814453 73.29510498046875 1.968311309814453 L 13.70430278778076 1.968311309814453 Z" fill="url(#gradient)" stroke="none" stroke-width="2" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
